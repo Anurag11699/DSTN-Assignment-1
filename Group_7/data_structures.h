63f9c833ff3491c1a025fbaee9223c6a91063d65
2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>
2020-03-10 10:25:25 +0530

    Adding DS for free frame list
    Signed-off-by: 2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>

--- Group_7/data_structures.h
+++ Group_7/data_structures.h
@@ -7,7 +7,22 @@ typedef struct frame_table_entry
 
 }frame_table_entry;
 
+
+typedef struct free_frame_list
+{
+	int frame_number;
+	struct free_frame_list *next;
+}free_frame_list;
+
+typedef struct free_frame_list_dummy_head
+{
+	int number_free_frames;
+	free_frame_list *next;
+}free_frame_list_dummy_head;
+
+
 typedef struct main_memory
 {
     frame_table_entry* frame_table;
+    free_frame_list_dummy_head *ffl_dummy_head;
 }main_memory;
--- Group_7/functions.c
+++ Group_7/functions.c
@@ -12,8 +12,14 @@ Output: {pointer to intialized main memory}
 */
 main_memory* initialize_main_memory(int main_memory_size, int frame_size)
 {
+    //find number of frames
     int number_of_frames=(main_memory_size/frame_size)*1024;
+
     main_memory* main_memory_object = (main_memory *)malloc(sizeof(main_memory));
+
+    //initialize frame table
     main_memory_object->frame_table=(frame_table_entry *)malloc(number_of_frames*sizeof(frame_table_entry));
+
+    //initialize free frame list;
     return main_memory_object;
 }
