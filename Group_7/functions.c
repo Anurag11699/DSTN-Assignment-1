410ece322380624a722b01594b5aded73eedfc98
2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>
2020-03-10 10:03:34 +0530

    Main Memory Implementation Started
    Signed-off-by: 2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>

--- Group_7/functions.c
+++ Group_7/functions.c
@@ -3,6 +3,13 @@
 #include <unistd.h>
 #include "functions.h"
 
+/*
+PreConditions
+Inputs: {main_memory_size in MB, frame_size in KB}
+
+PostConditions
+Output: {pointer to intialized main memory}
+*/
 main_memory* initialize_main_memory(int main_memory_size, int frame_size)
 {
 
