6778f83740a86da3f9cc183259141abd049ce3d4
2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>
2020-03-12 15:52:08 +0530

    Updating L2 Cache
    Signed-off-by: 2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>

--- Group_7/data_structures.h
+++ Group_7/data_structures.h
@@ -86,6 +86,8 @@ typedef struct L1_cache_entry
     unsigned int valid_set_3:1;
     unsigned int tag_set_3:14;
     char data_set_3[32];
+
+    int LRU_square_matrix[4][4];
 }L1_cache_entry;
 
 typedef struct L1_cache
@@ -96,68 +98,84 @@ typedef struct L1_cache
 typedef struct L2_cache_entry
 {
     unsigned int valid_set_0:1;
-    unsigned int tag_set_0;
+    unsigned int tag_set_0:14;
     char data_set_0[32];
+    unsigned int LFU_counter_0:16;
 
     unsigned int valid_set_1:1;
-    unsigned int tag_set_1;
+    unsigned int tag_set_1:14;
     char data_set_1[32];
+    unsigned int LFU_counter_1:16;
 
     unsigned int valid_set_2:1;
-    unsigned int tag_set_2;
+    unsigned int tag_set_2:14;
     char data_set_2[32];
+    unsigned int LFU_counter_2:16;
 
     unsigned int valid_set_3:1;
-    unsigned int tag_set_3;
+    unsigned int tag_set_3:14;
     char data_set_3[32];
+    unsigned int LFU_counter_3:16;
 
     unsigned int valid_set_4:1;
-    unsigned int tag_set_4;
+    unsigned int tag_set_4:14;
     char data_set_4[32];
+    unsigned int LFU_counter_4:16;
 
     unsigned int valid_set_5:1;
-    unsigned int tag_set_5;
+    unsigned int tag_set_5:14;
     char data_set_5[32];
+    unsigned int LFU_counter_5:16;
 
     unsigned int valid_set_6:1;
-    unsigned int tag_set_6;
+    unsigned int tag_set_6:14;
     char data_set_6[32];
+    unsigned int LFU_counter_6:16;
 
     unsigned int valid_set_7:1;
-    unsigned int tag_set_7;
+    unsigned int tag_set_7:14;
     char data_set_7[32];
+    unsigned int LFU_counter_7:16;
 
     unsigned int valid_set_8:1;
-    unsigned int tag_set_8;
+    unsigned int tag_set_8:14;
     char data_set_8[32];
+    unsigned int LFU_counter_8:16;
 
     unsigned int valid_set_9:1;
-    unsigned int tag_set_9;
+    unsigned int tag_set_9:14;
     char data_set_9[32];
+    unsigned int LFU_counter_10:16;
 
     unsigned int valid_set_10:1;
-    unsigned int tag_set_10;
+    unsigned int tag_set_10:14;
     char data_set_10[32];
+    unsigned int LFU_counter_10:16;
 
     unsigned int valid_set_11:1;
-    unsigned int tag_set_11;
+    unsigned int tag_set_11:14;
     char data_set_11[32];
+    unsigned int LFU_counter_11:16;
 
     unsigned int valid_set_12:1;
-    unsigned int tag_set_12;
+    unsigned int tag_set_12:14;
     char data_set_12[32];
+    unsigned int LFU_counter_12:16;
 
     unsigned int valid_set_13:1;
-    unsigned int tag_set_13;
+    unsigned int tag_set_13:14;
     char data_set_13[32];
+    unsigned int LFU_counter_13:16;
 
     unsigned int valid_set_14:1;
-    unsigned int tag_set_14;
+    unsigned int tag_set_14:14;
     char data_set_14[32];
+    unsigned int LFU_counter_14:16;
 
     unsigned int valid_set_15:1;
-    unsigned int tag_set_15;
+    unsigned int tag_set_15:14;
     char data_set_15[32];
+    unsigned int LFU_counter_15:16;
 
 }L2_cache_entry;
 
