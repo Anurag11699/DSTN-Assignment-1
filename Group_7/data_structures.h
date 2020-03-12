4fd9c3a5e5b0ebdd88f1e9f0d6149d53b8601d1f
2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>
2020-03-12 16:11:35 +0530

    Added L2 Cache Write Buffer
    Signed-off-by: 2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>

--- Group_7/data_structures.h
+++ Group_7/data_structures.h
@@ -202,8 +202,15 @@ typedef struct L2_cache
 
 typedef struct L2_cache_write_buffer_entry
 {
-
+    unsigned int index:6;
+    unsigned int tag:14;
+    char data[32];
 }L2_cache_write_buffer_entry;
+
+typedef struct L2_cache_write_buffer
+{
+    L2_cache_write_buffer_entry *L2_cache_write_buffer_entries;
+}L2_cache_write_buffer;
 
 typedef struct page_table_entry
 {
