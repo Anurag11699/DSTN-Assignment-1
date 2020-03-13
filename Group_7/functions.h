819686fc4c25ab771d2eb4205e7249e0588ecb4f
2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>
2020-03-13 07:05:11 +0530

    added tlb_flush
    Signed-off-by: 2017A7PS1923G <f20171923@goa.bits-pilani.ac.in>

--- Group_7/functions.h
+++ Group_7/functions.h
@@ -3,12 +3,17 @@
 #include "tlb_data_structures.h"
 #include "general_data_structures.h"
 
+//main memory functions
+extern main_memory* initialize_main_memory(int, int);
 extern void add_free_frame(free_frame_list_dummy_head *, free_frame **, int);
 extern int remove_free_frame(free_frame_list_dummy_head *);
-
 extern void add_used_frame(used_frame_list_dummy_head *, used_frame **, int);
 extern int remove_used_frame(used_frame_list_dummy_head *, used_frame **);
 
-extern main_memory* initialize_main_memory(int, int);
-extern tlb* initialize_tlb(int);
+//cache functions
 extern L1_cache* initialize_L1_cache();
+
+
+//tlb functions
+extern tlb* initialize_tlb(int);
+extern void tlb_flush(tlb*, int);
--- Group_7/tlb_functions.c
+++ Group_7/tlb_functions.c
@@ -10,3 +10,11 @@ tlb* initialize_tlb(int number_of_entries)
 
     return tlb_object;
 }
+
+void tlb_flush(tlb* tlb_object, int number_of_entries)
+{
+    for(int i=0;i<number_of_entries;i++)
+    {
+        tlb_object->tlb_entries[i].valid=0;
+    }
+}
